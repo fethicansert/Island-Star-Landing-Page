1d624cdf2831da911474bd40be27c83099d250b6
fethi <fethisert0@gmail.com>
2024-10-16 17:02:28 +0300

    mobile hero page finished animations added

--- newIndex.js
+++ newIndex.js
@@ -10,8 +10,10 @@ const burgerMenuBottom = document.getElementById('burger-menu-icon-bottom')
 const heroImageContainer = document.getElementById('image-slider-container');
 const arrowCircleLeft = document.getElementById('arrow-circle-left');
 const arrowCircleRight = document.getElementById('arrow-circle-right');
+const imagePositions = document.querySelectorAll('.image-positon');
 
-
+//Current position of image
+let currentPosition = 0;
 
 // TOOGLE active classes when burger menu clicked.
 const activeHeaderNav = () => {
@@ -22,26 +24,46 @@ const activeHeaderNav = () => {
     headerListItems.forEach(item => item.classList.toggle('active'));
 }
 
-headerBurgerMenu.addEventListener('click', activeHeaderNav);
 
-let currentPosition = 0;
+//LISTINERS
+
+headerBurgerMenu.addEventListener('click', activeHeaderNav);
 
 arrowCircleLeft.addEventListener('click', slideLeft);
 
 arrowCircleRight.addEventListener('click', slideRight);
 
+
+//slide left to image 
 function slideLeft() {
     console.log(currentPosition);
     if (currentPosition > 0) {
-        currentPosition = currentPosition - 100;;
+        currentPosition = currentPosition - 100;
         heroImageContainer.style.transform = `translateX(-${currentPosition}%)`;
+        changeImagePosition(currentPosition);
     }
 }
 
+//slide right to image 
 function slideRight() {
     console.log(currentPosition);
     if (currentPosition < 200) {
         currentPosition = currentPosition + 100;
         heroImageContainer.style.transform = `translateX(-${currentPosition}%)`;
+        changeImagePosition(currentPosition);
     }
+}
+
+//change image positions cirlces styles
+function changeImagePosition(currentPosition) {
+    imagePositions.forEach((position, index) => {
+        position.classList.remove('active');
+        if (index === 0 && currentPosition === 0) {
+            position.classList.add('active');
+        } else if (index === 1 && currentPosition === 100) {
+            position.classList.add('active');
+        } else if (index === 2 && currentPosition === 200) {
+            position.classList.add('active');
+        }
+    });
 }
