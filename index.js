c3d36c93db3793dc0a1542e477bdd3ea737ea933
fethi <fethisert0@gmail.com>
2024-10-18 03:26:22 +0300

    image optimaztions added

--- index.js
+++ index.js
@@ -1,26 +1,77 @@
-const heroImageContainer = document.getElementById('hero-image-container');
+//HEADER NAV ACTIVE VARAIBLES
+const headerBurgerMenu = document.getElementById('header-burger-menu');
+const headerNav = document.getElementById('header-nav');
+const headerListItems = document.querySelectorAll('.header-nav li');
+const lineBreak = document.getElementById('line-break');
+const burgerMenuTop = document.getElementById('burger-menu-icon-top');
+const burgerMenuBottom = document.getElementById('burger-menu-icon-bottom')
+
+const headerLists = document.querySelectorAll('.header-nav ul');
+const arrowsContainer = document.getElementById('arrows-container');
+const heroContainer = document.getElementById('hero-container');
+
+
+//IMAGE SLIDER VARIABLES
+const heroImageContainer = document.getElementById('image-slider-container');
 const arrowCircleLeft = document.getElementById('arrow-circle-left');
 const arrowCircleRight = document.getElementById('arrow-circle-right');
+const imagePositions = document.querySelectorAll('.image-positon');
+
+//Current position of image
+let currentPosition = 0;
+
+// TOOGLE active classes when burger menu clicked.
+const activeHeaderNav = () => {
+    headerNav.classList.toggle('active');
+    lineBreak.classList.toggle('active');
+    burgerMenuBottom.classList.toggle('active');
+    burgerMenuTop.classList.toggle('active');
+    arrowsContainer.classList.toggle('hide')
+    heroContainer.classList.toggle('hide');
+
+    headerLists.forEach(item => item.classList.toggle('opacity'));
+}
 
 
+//LISTINERS
+
+headerBurgerMenu.addEventListener('click', activeHeaderNav);
 
 arrowCircleLeft.addEventListener('click', slideLeft);
 
 arrowCircleRight.addEventListener('click', slideRight);
 
-let currentPosition = 0;
 
+//slide left to image 
 function slideLeft() {
+    console.log(currentPosition);
     if (currentPosition > 0) {
-        currentPosition = currentPosition - 100;;
+        currentPosition = currentPosition - 100;
         heroImageContainer.style.transform = `translateX(-${currentPosition}%)`;
+        changeImagePosition(currentPosition);
     }
 }
 
+//slide right to image 
 function slideRight() {
+    console.log(currentPosition);
     if (currentPosition < 200) {
         currentPosition = currentPosition + 100;
         heroImageContainer.style.transform = `translateX(-${currentPosition}%)`;
-        console.log(currentPosition);
+        changeImagePosition(currentPosition);
     }
+}
+
+//change image positions cirlces styles
+function changeImagePosition(currentPosition) {
+    imagePositions.forEach((position, index) => {
+        position.classList.remove('active');
+        if (index === 0 && currentPosition === 0) {
+            position.classList.add('active');
+        } else if (index === 1 && currentPosition === 100) {
+            position.classList.add('active');
+        } else if (index === 2 && currentPosition === 200) {
+            position.classList.add('active');
+        }
+    });
 }
