c601756f6c0ca4d869378b782d81dbd8cc41d4b2
fethi <fethisert0@gmail.com>
2024-10-31 16:01:36 +0200

    js and some images optimazed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//HEADER NAV ACTIVE VARAIBLES
+//VARIABLES
 const headerBurgerMenu = document.getElementById('header-burger-menu');
 const headerNav = document.getElementById('header-nav');
 const headerListItems = document.querySelectorAll('.header-nav li');
@@ -10,13 +10,10 @@ const headerLists = document.querySelectorAll('.header-nav ul');
 const arrowsContainer = document.getElementById('arrows-container');
 const heroContainer = document.getElementById('hero-container');
 
-
-//IMAGE SLIDER VARIABLES
 const heroImageContainer = document.getElementById('image-slider-container');
 const arrowCircleLeft = document.getElementById('arrow-circle-left');
 const arrowCircleRight = document.getElementById('arrow-circle-right');
 const imagePositions = document.querySelectorAll('.image-positon');
-
 
 const projectDetailArrowDown = document.querySelectorAll('.project-detail-arrow');
 const projectSubDetails = document.querySelectorAll('.projects-sub-detail');
@@ -26,14 +23,33 @@ const projectPlanButtons = document.querySelectorAll('.house-plan-button');
 const projectPlanImagePositons = document.querySelectorAll('.project-plan-image-positon');
 
 
+const projectImageButtons = document.querySelectorAll('.project-image-button');
+const projectImagePositons = document.querySelectorAll('.project-image-positon');
 
 let homeCurrentImagePosition = 0;
 let planCurrentImagePosition = 0;
+let projectCurrentImagePosition = 0;
+
+//LISTINERS
+headerBurgerMenu.addEventListener('click', activeHeaderNav);
+
+arrowCircleLeft.addEventListener('click', slideLeft);
+
+arrowCircleRight.addEventListener('click', slideRight);
 
 
+projectPlanButtons[0].addEventListener('click', () =>
+    projectSlideLeft(planCurrentImagePosition, setProjectImage, 'plan-slider'));
 
-projectPlanButtons[0].addEventListener('click', planImageSlideLeft);
-projectPlanButtons[1].addEventListener('click', planImageSlideRight);
+projectPlanButtons[1].addEventListener('click', () =>
+    projectSlideRight(planCurrentImagePosition, setProjectImage, 'plan-slider', 4));
+
+
+projectImageButtons[0].addEventListener('click', () =>
+    projectSlideLeft(projectCurrentImagePosition, setProjectImage, 'project-image-slider'));
+
+projectImageButtons[1].addEventListener('click', () =>
+    projectSlideRight(projectCurrentImagePosition, setProjectImage, 'project-image-slider', 3));
 
 
 projectDetailArrowDown.forEach((item, index) => {
@@ -44,16 +60,12 @@ projectDetailArrowDown.forEach((item, index) => {
     });
 })
 
+//OBSERVERS
 const intersectionOptions = {
     root: null,
     thresold: 0,
     rootMargin: '0px 0px -7% 0px'
 }
-
-
-//OBSERVERS
-
-//Section Intersection Observer => helps me to animate elements when user intersect with observed element or section
 const observer = new IntersectionObserver(function (entries, observer) {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -63,16 +75,13 @@ const observer = new IntersectionObserver(function (entries, observer) {
         }
     })
 }, intersectionOptions);
-
 observer.observe(document.querySelector('.projects-section'));
-
 
 const intersectionOptions2 = {
     root: null,
     thresold: 0,
     rootMargin: '0px 0px -5% 0px'
 }
-
 const observer2 = new IntersectionObserver(function (entries, observer) {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -84,62 +93,55 @@ const observer2 = new IntersectionObserver(function (entries, observer) {
             entry.target.firstElementChild.style.transform = 'translateY(0%)';
             if (entry.target.className === 'project-deal-title style-title') return observer2.disconnect();
         }
-
     });
 }, intersectionOptions2);
-
 document.querySelectorAll('.projects-section h3').forEach(header => observer2.observe(header));
 
 
-
-
-
+//FUNCTIONS
 // TOOGLE active classes when burger menu clicked.
-const activeHeaderNav = () => {
+function activeHeaderNav() {
     headerNav.classList.toggle('active');
     lineBreak.classList.toggle('active');
     burgerMenuBottom.classList.toggle('active');
     burgerMenuTop.classList.toggle('active');
     arrowsContainer.classList.toggle('hide')
     heroContainer.classList.toggle('hide');
-
     headerLists.forEach(item => item.classList.toggle('opacity'));
 }
 
-//LISTINERS
-headerBurgerMenu.addEventListener('click', activeHeaderNav);
-
-arrowCircleLeft.addEventListener('click', slideLeft);
-
-arrowCircleRight.addEventListener('click', slideRight);
-
-
-//FUNCTIONS
-
-function planImageSlideLeft() {
-    if (planCurrentImagePosition > 0) planCurrentImagePosition -= 1;
-    setPlanImage(planCurrentImagePosition);
+//Project Section Sliders Slide LEFT
+function projectSlideLeft(position, setSlideImage, slider) {
+    if (position > 0) slider === 'plan-slider' ? planCurrentImagePosition -= 1 : projectCurrentImagePosition -= 1;
+    const options = getOptions(slider);
+    setSlideImage(options.index, options.positions, options.image, options.imageGroup);
 }
 
-function planImageSlideRight() {
-    if (planCurrentImagePosition < 3) planCurrentImagePosition += 1;
-    console.log(planCurrentImagePosition);
-    setPlanImage(planCurrentImagePosition);
+//Project Section Sliders Slide RIGHT
+function projectSlideRight(position, setSlideImage, slider, maxImageSize) {
+    if (position < maxImageSize - 1) slider === 'plan-slider' ? planCurrentImagePosition += 1 : projectCurrentImagePosition += 1;
+    const options = getOptions(slider);
+    setSlideImage(options.index, options.positions, options.image, options.imageGroup);
 }
 
-function setPlanImage(index) {
-    document.querySelector('.project-plan-image').src = `./images/house_plan${index}.jpg`;
-    projectPlanImagePositons.forEach(item => {
-        if (item.classList.contains('active')) {
-            item.classList.remove('active');
-        }
-    })
-    projectPlanImagePositons[index].classList.add('active');
+//Project Section Sliders Set Image
+function setProjectImage(index, positions, image, imageGroup) {
+    document.querySelector(`.${image}`).src = `./images/${imageGroup}${index}.webp`;
+    positions.forEach(position => position.classList.remove('active'))
+    positions[index].classList.add('active');
 }
 
+//Getl Slider Options decide which image element and which image group...
+function getOptions(slider) {
+    return {
+        index: slider === 'plan-slider' ? planCurrentImagePosition : projectCurrentImagePosition,
+        positions: slider === 'plan-slider' ? projectPlanImagePositons : projectImagePositons,
+        image: slider === 'plan-slider' ? 'project-plan-image' : 'project-image',
+        imageGroup: slider === 'plan-slider' ? 'house_plan' : 'project_image'
+    }
+}
 
-
-//slide left to image 
+//home slide left to image 
 function slideLeft() {
     console.log(homeCurrentImagePosition);
     if (homeCurrentImagePosition > 0) {
@@ -149,7 +151,7 @@ function slideLeft() {
     }
 }
 
-//slide right to image 
+//home slide right to image 
 function slideRight() {
     console.log(homeCurrentImagePosition);
     if (homeCurrentImagePosition < 200) {
@@ -159,7 +161,7 @@ function slideRight() {
     }
 }
 
-//change image positions cirlces styles
+//home change image positions cirlces styles
 function changeImagePosition(homeCurrentImagePosition) {
     imagePositions.forEach((position, index) => {
         position.classList.remove('active');
@@ -173,7 +175,7 @@ function changeImagePosition(homeCurrentImagePosition) {
     });
 };
 
-
+//Button effect syles
 document.querySelectorAll('.hero-btn').forEach(button => {
     button.addEventListener('touchstart', () => {
         button.classList.add('active');
